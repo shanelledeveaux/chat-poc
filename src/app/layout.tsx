4a3614ca64f79a13f22782386b8c6e12c9be6e06
Shanelle Webster <webstershanelle@gmail.com>
2025-09-08 20:00:11 -0500

    fix: build issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { NavBar } from "./components/NavBar/NavBar";
+import { Suspense } from "react";
 
 export default function RootLayout({
   children,
@@ -9,7 +10,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-gray-950 text-white h-screen">
-        <main className="px-4 pt-4 pb-14 h-full">{children}</main>
+        <Suspense>
+          <main className="px-4 pt-4 pb-14 h-full">{children}</main>
+        </Suspense>
         <NavBar />
       </body>
     </html>
